e6adddd5b2eed0902114c94a78365763d1eddece
Sandro <31536101+sandromund@users.noreply.github.com>
2023-10-02 22:29:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from dotenv import dotenv_values
 
 from src.messages import Connection
+from src.view import Game
 
 if __name__ == '__main__':
     secrets = dotenv_values(".env")
-    tcp_ip = secrets.get("TCP_IP")
-    tcp_port = secrets.get("TCP_PORT")
-
-    conn = Connection(tcp_ip=tcp_ip, tcp_port=tcp_port)
-    conn.connect()
-    conn.sent_move("quit")
-    state, score, board = conn.decode_server_message(conn.data)
-    conn.disconnect()
+    game = Game(connection=Connection(tcp_ip=secrets.get("TCP_IP"),
+                                      tcp_port=secrets.get("TCP_PORT")))
+    game.run()
